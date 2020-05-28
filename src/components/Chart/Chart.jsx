958c6463cc13b7e524b39cc259f80acc56dcb34c
swapneal <52267392+swapneal@users.noreply.github.com>
2020-05-28 15:30:10 -0700

    Chart added

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
 
+import styles from './Chart.module.css';
+
+import { fetchDailyData } from '../../api';
 import Spinner from '../Spinner/Spinner';
 
 const Chart = () => {
-	return (
-		<div>
-			<h1>Chart</h1>
-			<Spinner />
-		</div>
+	const [dailyData, setDailyData] = useState([]);
+
+	useEffect(() => {
+		const fetchAPI = async () => {
+			const dailyData = await fetchDailyData();
+			setDailyData(dailyData);
+		};
+		fetchAPI();
+	}, []);
+
+	const lineChart = dailyData[0] ? (
+		<Line
+			data={{
+				labels: dailyData.map(({ date }) => date),
+				datasets: [
+					{
+						data: dailyData.map(({ confirmed }) => confirmed),
+						label: 'Infected',
+						borderColor: '#3333ff',
+						fill: true,
+					},
+					{
+						data: dailyData.map(({ deaths }) => deaths),
+						label: 'Deaths',
+						borderColor: 'red',
+						backgroundColor: 'rgba(255,0,0,0.5)',
+						fill: true,
+					},
+				],
+			}}
+		/>
+	) : (
+		<Spinner />
 	);
+
+	return <div className={styles.container}>{lineChart}</div>;
 };
 
 export default Chart;
