cc563482c84d3fd29c738243a041860c6989fda3
swapneal <52267392+swapneal@users.noreply.github.com>
2020-05-31 11:18:30 -0700

    country picker moved up

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import styles from './App.module.css';
+import React from 'react'
+import styles from './App.module.css'
 
-import { fetchData } from './api';
+import { fetchData } from './api'
 
-import { Cards, Chart, CountryPicker } from './components';
-import image from './images/image.png';
+import { Cards, Chart, CountryPicker } from './components'
+import image from './images/image.png'
 
 class App extends React.Component {
-	state = {
-		data: {},
-		country: '',
-	};
-	async componentDidMount() {
-		const data = await fetchData();
-		this.setState({ data });
-	}
+  state = {
+    data: {},
+    country: ''
+  }
+  async componentDidMount () {
+    const data = await fetchData()
+    this.setState({ data })
+  }
 
-	handleCountryChange = async (country) => {
-		const data = await fetchData(country);
-		this.setState({
-			data,
-			country,
-		});
-	};
-	render() {
-		const { data, country } = this.state;
-		return (
-			<div className={styles.container}>
-				<img className={styles.image} src={image} alt="COVID-19" />
-				<Cards data={data} />
-				<CountryPicker handleCountryChange={this.handleCountryChange} />
-				<Chart data={data} country={country} />
-			</div>
-		);
-	}
+  handleCountryChange = async country => {
+    const data = await fetchData(country)
+    this.setState({
+      data,
+      country
+    })
+  }
+  render () {
+    const { data, country } = this.state
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt='COVID-19' />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Cards data={data} />
+        <Chart data={data} country={country} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
